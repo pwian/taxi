991947809624a412bfae7490f3095ad9a6fa27c0
Aleksandr Ivakin <aleksandr_ivakin@epam.com>
2021-06-28 20:30:22 +0300

    add empty controller

---
+++ taxiService/src/main/java/com/taxi/taxiService/TaxiApplication.java
@@ -0,0 +1,10 @@
+package com.taxi.taxiService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiApplication {
+
+    public static void main(String[] args) { SpringApplication.run(TaxiApplication.class, args); }
+}
---
+++ taxiService/src/main/java/com/taxi/taxiService/controller/TaxiController.java
@@ -0,0 +1,9 @@
+package com.taxi.taxiService.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaxiController {
+
+
+}
