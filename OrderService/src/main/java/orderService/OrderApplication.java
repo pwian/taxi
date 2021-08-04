2f65e5640143dcd41ea3f1066f059b9934e232e3
Aleksandr Ivakin <aleksandr_ivakin@epam.com>
2021-08-04 21:08:56 +0300

    fix services

--- OrderService/src/main/java/orderService/OrderApplication.java
+++ OrderService/src/main/java/orderService/OrderApplication.java
@@ -1,7 +1,17 @@
 package orderService;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
  * @author Aleksandr Ivakin
  **/
+@SpringBootApplication
+@EnableEurekaServer
 public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
 }
